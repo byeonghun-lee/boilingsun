e537aeb2440b4bf67c4afe9df7b8385e0b7239fd
byeonghun <byeonghun08@gmail.com>
2019-05-30 01:03:56 +0900

    [Modified]api card route 설정

--- boilingsun-backend/src/api/cards/index.js
+++ boilingsun-backend/src/api/cards/index.js
@@ -6,6 +6,9 @@ const cards = new Router();
 
 cards.get("/", cardsCtrl.list);
 cards.post("/", cardsCtrl.write);
+cards.get("/:id", cardsCtrl.read);
+cards.delete("/:id", cardsCtrl.remove);
+cards.patch("/:id", cardsCtrl.update);
 
 module.exports = cards;
 
