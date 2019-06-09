f7dc389dc1c3efbf8e5713b6cb95ab2bf4928e38
byeonghun <byeonghun08@gmail.com>
2019-06-09 22:22:12 +0900

    [Modified]object 타입 체크하는 미들웨어 추가

--- boilingsun-backend/src/api/cards/index.js
+++ boilingsun-backend/src/api/cards/index.js
@@ -6,9 +6,9 @@ const cards = new Router();
 
 cards.get("/", cardsCtrl.list);
 cards.post("/", cardsCtrl.write);
-cards.get("/:id", cardsCtrl.read);
-cards.delete("/:id", cardsCtrl.remove);
-cards.patch("/:id", cardsCtrl.update);
+cards.get("/:id", cardsCtrl.checkObjectId, cardsCtrl.read);
+cards.delete("/:id", cardsCtrl.checkObjectId, cardsCtrl.remove);
+cards.patch("/:id", cardsCtrl.checkObjectId, cardsCtrl.update);
 
 module.exports = cards;
 
