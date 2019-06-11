6cde15444f651ad54324d34447ad19fba456b511
byeonghun <byeonghun08@gmail.com>
2019-06-12 00:47:19 +0900

    [Modified]category route 연결

--- boilingsun-backend/src/api/index.js
+++ boilingsun-backend/src/api/index.js
@@ -1,8 +1,10 @@
 const Router = require("koa-router");
 const cards = require("./cards");
+const category = require("./category");
 
 const api = new Router();
 
 api.use("/cards", cards.routes());
+api.use("/category", category.routes());
 
 module.exports = api;
